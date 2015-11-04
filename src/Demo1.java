c4678250767f0b3dc2a9196c16aee6f4f808eaa4
Adamantcheese <random123x@gmail.com>
2015-11-04 13:05:03 -0800

    buttholes

--- src/Demo1.java
+++ src/Demo1.java
@@ -10,38 +10,61 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 /**
- * Created by Jacob on 10/21/2015.
+ * Original created by Jacob on 10/21/2015.
+ * Modified by David
  */
 public class Demo1 {
     public static void main(String[] args) {
         //Setup the encryption scheme - this part goes on the server
+        System.out.println("Initializing PKG ... ");
         SecureRandom random = new SecureRandom();
         Pairing pair = PairingFactory.ssTate(128, 512, random);
         BFCipher cipher = new BFCipher();
         KeyPair masterPair = cipher.setup(pair, random);
 
         //Grab a private/public key from the scheme with a given ID - query the server for these (see below)
-        KeyPair userPair = cipher.extract(masterPair, "testName@cpp.edu", random);
+        System.out.println("Please enter the recipient's ID: ");
+        Scanner kb = new Scanner(System.in);
+        String receiptID = kb.nextLine();
+        KeyPair userPair = cipher.extract(masterPair, receiptID, random);
         BFUserPublicKey publicKey = (BFUserPublicKey) userPair.getPublic();
         BFUserPrivateKey privateKey = (BFUserPrivateKey) userPair.getPrivate();
+        System.out.println("Received " + receiptID + "'s public key.");
+        kb.nextLine();
 
         //Grab some test input to encrypt with a person's public key
-        Scanner kb = new Scanner(System.in);
         System.out.print("Enter a string to encrypt: ");
         String testLine = kb.nextLine();
-        kb.close();
 
         //Encrypt and print - query the server for the public key
+        System.out.println("Encrypting message ... ");
         BFCtext cipherText = cipher.encrypt(publicKey, testLine.getBytes(), random);
         System.out.println("Ciphertext: ");
         System.out.println("U: " + cipherText.getU()
                 + "\nV: " + new String(cipherText.getV())
                 + "\nW: " + new String(cipherText.getW()));
+        kb.nextLine();
+        kb.close();
 
         //Decrypt and print - query the server for the private key AND DISALLOW FUTURE REQUESTS
+        System.out.println("\nDecrypting message ... ");
         byte[] plainText = cipher.decrypt(cipherText, privateKey);
-        System.out.println();
         System.out.println("Decrypted plaintext: ");
         System.out.println(new String(plainText));
+
+        //Reencrypt again
+        System.out.println("Re-encrypting message ... ");
+        cipherText = cipher.encrypt(publicKey, plainText, random);
+        System.out.println("Ciphertext: ");
+        System.out.println("U: " + cipherText.getU()
+                + "\nV: " + new String(cipherText.getV())
+                + "\nW: " + new String(cipherText.getW()));
+
+        //Decrypt again
+        System.out.println("\nDecrypting message ... ");
+        plainText = cipher.decrypt(cipherText, privateKey);
+        System.out.println("Decrypted plaintext: ");
+        System.out.println(new String(plainText));
+
     }
 }
